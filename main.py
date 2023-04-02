184833c50c986aa445f509620063a52682ee3ed0
RihardsNolmanis <125802711+RihardsNolmanis@users.noreply.github.com>
2023-04-02 16:37:30 +0000

    Rihards Nolmanis 221RDB431 kods strādā, bet inputiem ir jābut bez atstarpēm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# python3
-
+#python3
+# Rihards Nolmanis 221RDB431
+## kods strādā, bet inputiem ir jābut bez atstarpēm.
 class Query:
     def __init__(self, query):
         self.type = query[0]
@@ -8,40 +9,25 @@ class Query:
             self.name = query[2]
 
 def read_queries():
-    n = int(input())
-    return [Query(input().split()) for i in range(n)]
+    n_new = int(input())
+    return [Query(input().split()) for i in range(n_new)]
 
 def write_responses(result):
     print('\n'.join(result))
 
 def process_queries(queries):
     result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    phonebook = {}
     for cur_query in queries:
         if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
+            phonebook[cur_query.number] = cur_query.name
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
-        else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+            if cur_query.number in phonebook:
+                del phonebook[cur_query.number]
+        else:   
+            response = phonebook.get(cur_query.number, 'not found')
             result.append(response)
     return result
 
 if __name__ == '__main__':
-    write_responses(process_queries(read_queries()))
-
+    write_responses(process_queries(read_queries()))    
